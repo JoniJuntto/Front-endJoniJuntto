65bdbf0765d8d57fc17461c987221b2c25b2c342
Joni Juntto <bgm043@myy.haaga-helia.fi>
2021-03-07 22:38:37 +0200

    omatehtävä korjaukset

--- omatehtava/src/App.js
+++ omatehtava/src/App.js
@@ -6,8 +6,20 @@ import { useForm } from "react-hook-form";
 
 
 function App() {
+
+  const paraStyle ={
+    
+    marginLeft: 400,
+    marginRight: 400,
+    marginBottom: 50,
+    marginTop: 50,
+    padding: 20,
+    fontSize: 20,
+  };
+  
+
   const [recipes, setRecipes] = useState([]);
-  const [value, setValue] = useState('');
+  var value = '';
 
   const getRecipes = async () => {
     const url = 'http://www.recipepuppy.com/api/?i=' + value;
@@ -15,9 +27,7 @@ function App() {
     try {
       const response = await fetch(url);
       var data = await response.json();
-      console.log(data.results)
       setRecipes(data.results);
-      console.log(data.results)
     } catch (error) {
       console.log('error', error);
     }
@@ -27,7 +37,7 @@ function App() {
 
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-    setValue(data.search);
+    value = data.search;
     getRecipes();
   }
 
@@ -36,11 +46,18 @@ function App() {
 
   return (
     <div className="App">
-      <RecipeList recipes={recipes} />
+      <h1>Resepti haku</h1>
+
+      <div  style={paraStyle}>
+        <p>Jos jääkaapin pohjalle on jäänyt jotain ainesosaa ylimääräiseksi,</p>
+        <p> voit hakea ainesosan nimellä herkullisia reseptejä, joihin sitä voit käyttää!</p>
+      </div>
+
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type='text' placeholder='Search' name='search' ref={register} />
+        <input type='text' placeholder='Search recipe with ingredient' name='search' ref={register} />
         <input type='submit' />
       </form>
+      <RecipeList recipes={recipes} />
     </div>
   );
 }
--- omatehtava/src/RecipeList.js
+++ omatehtava/src/RecipeList.js
@@ -4,14 +4,31 @@ import React from 'react';
      if (props.recipes.length === 0){
          return (<p>Ei sisältöä</p>);
      }
+
+     const recipeStyle = {
+        borderStyle: 'solid',
+        marginBottom: 30,
+        marginTop: 30,
+        marginLeft: 700,
+        marginRight: 700,
+        padding: 30,
+     };
+
+     const imgStyle = {
+         width: 150,
+         height: 150,
+     };
+
      return(
          <div>
              {
                  props.recipes.map(recipe =>{
+                     console.log(recipe)
+                    var imageUrl = recipe.thumbnail.toString();
                      return(
-                         <div key={recipe.id}>
+                         <div style={recipeStyle} key={recipe.id}>
                              <a href={recipe.href}>{recipe.title}</a><br/>
-                             <img alt='Ruokakuva' source={recipe.thumbnail}/>
+                             <img style={imgStyle} src={imageUrl} alt='Ruokakuva' />
                          </div>
                      );
                  })
